75bfc54a943aab59b496a05c2cba0e13e0943f4e
davidGlobant <david.villanueva@grey.com>
2019-06-06 12:22:19 -0500

    Refactor of routes and configuration

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
 'use strict'
 
-const express = require ('express')
-const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const app =  require('./app')
+const config = require('./config')
 
-const app = express()
-const port = process.env.PORT || 3000
-
-const ProductCtrl = require('./controllers/product')
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.get('/api/product', ProductCtrl.getProducts)
-
-app.get('/api/product/:productId', ProductCtrl.getProduct)
-
-app.post('/api/product', ProductCtrl.createProduct)
-
-app.put('/api/product/:productId', ProductCtrl.updateProduct)
-
-app.delete('/api/product/:productId', ProductCtrl.deleteProduct)
-
-mongoose.connect('mongodb://localhost:27017/shopCenter', {useNewUrlParser: true}, (err, res) => {
+mongoose.connect(config.db, {useNewUrlParser: true}, (err, res) => {
   if(err) {
     return console.error('Error trying to connect to db', err)
   }
   console.log('Connection to db ready to go')
 
-  
-  app.listen(port, () => {
-    console.log(`API REST working on http://localhost:${port}`)
+  app.listen(config.port, () => {
+    console.log(`API REST working on http://localhost:${config.port}`)
   })
 })
 
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express')
+const api =  express.Router()
+const ProductCtrl = require('../controllers/product')
+
+api.get('/product', ProductCtrl.getProducts)
+api.get('/product/:productId', ProductCtrl.getProduct)
+api.post('/product', ProductCtrl.createProduct)
+api.put('/product/:productId', ProductCtrl.updateProduct)
+api.delete('/product/:productId', ProductCtrl.deleteProduct)
+
+module.exports = api
