f8d82ab782e6f0349d497fbb0aba3d64fc3ab62d
davidGlobant <david.villanueva@grey.com>
2019-06-25 18:56:47 -0500

    'Heroku'

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: process.env.PORT || 3000,"",
+  port: process.env.PORT || 3000,
   db: 'mongodb://heroku_1mp8tvhc:1.Katakuri@ds243607.mlab.com:43607/heroku_1mp8tvhc',
   SECRET_TOKEN: 'SECRET_TOKEN'
 }
