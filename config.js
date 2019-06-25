dca58a2849a0800f0657e4ec2d783f1906f1548f
davidGlobant <david.villanueva@grey.com>
2019-06-25 18:48:42 -0500

    'Heroku'

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: process.env.PORT || 3000,
-  db: process.env.MONGODB || 'mongodb://localhost:27017/shopCenter',
+  port: process.env.PORT || 3000,"",
+  db: 'mongodb://heroku_1mp8tvhc:1.Katakuri@ds243607.mlab.com:43607/heroku_1mp8tvhc',
   SECRET_TOKEN: 'SECRET_TOKEN'
 }
